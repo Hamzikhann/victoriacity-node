8df6b5ecbd22ee15780b2505636b5079bf7236d5
Hamza Qasim <hamzaqasim.c@gmail.com>
2024-05-11 04:05:57 +0500

    payment plan getting fixed

--- app/http/controllers/bookingController.js
+++ app/http/controllers/bookingController.js
@@ -599,7 +599,7 @@ class BookingController {
 			Date.prototype.addMonths = function (value) {
 				var n = this.getDate();
 				this.setDate(1);
-				this.setMonth(this.getMonth() + value);
+				this.setMonth(this.getMonth());
 				this.setDate(Math.min(n, this.getDaysInMonth()));
 				return this;
 			};
